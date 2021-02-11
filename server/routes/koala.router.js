6c7c1845805e68209e78a5a1a6f997cf6541be78
patrickrnelson <patrickrnelson1@gmail.com>
2021-02-11 14:39:02 -0600

    server GET route is setup and tested successfully

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const koalaRouter = express.Router();
-const pool = require('../modules/pool');
 
 // DB CONNECTION
-
+const pool = require('../modules/pool');
 
 // GET
-
-
+koalaRouter.get('/', (req, res) => {
+  // do a DB query
+  pool
+    .query(
+      `SELECT * FROM "koalas"
+            ORDER BY "id" ASC`
+    )
+    .then(function (dbRes) {
+      // console.log(dbRes.rows);
+      // send all koalas to client
+      res.send(dbRes.rows);
+    })
+    // or handle DB errors
+    .catch(function (err) {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
 // POST
 
-
 // PUT
-
 
 // DELETE
 
