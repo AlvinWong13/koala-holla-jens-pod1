a8896cd3a7148cae72ad0ca99c789856413131d4
patrickrnelson <patrickrnelson1@gmail.com>
2021-02-11 14:57:11 -0600

    got started on the POST route

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -4,7 +4,7 @@ const koalaRouter = express.Router();
 // DB CONNECTION
 const pool = require('../modules/pool');
 
-// GET
+// *** GET
 koalaRouter.get('/', (req, res) => {
   // do a DB query
   pool
@@ -23,8 +23,44 @@ koalaRouter.get('/', (req, res) => {
       res.sendStatus(500);
     });
 });
-// POST
 
+// *** POST
+router.post('/', (req, res) => {
+  /*
+  Query should look like this:
+
+  INSERT INTO "songs"
+    ("artist", "track", "published", "rank")
+  VALUES
+    ('some artist', 'tracky', '1-1-1970', 7);
+
+  */
+
+  console.log('req.body', req.body);
+
+  // do a DB query
+  pool
+    .query(
+      `
+    INSERT INTO "koalas"
+    ("name", "gender", "age", "transfer", "notes")
+    VALUES
+    ('${req.body.name}', '${req.body.gender}', '${req.body.age}', ${req.body.transfer}, ${req.body.notes});
+    `
+    )
+    // could be .then(dbRes => {.... })
+    .then(function (dbRes) {
+      // console.log(dbRes.rows);
+      res.sendStatus(201);
+    })
+    // or handle DB errors
+    .catch(function (err) {
+      console.log(err);
+      // don't ghost your client!
+      // send a status
+      res.sendStatus(500);
+    });
+});
 // PUT
 
 // DELETE
